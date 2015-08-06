f3c936a018089790059cb82e1559370c773f1dac
kritinemkul <nemkulk@gmail.com>
2015-08-06 13:31:28 +0545

    update secor vocabulary

--- sql/update.sql
+++ sql/update.sql
@@ -5,3 +5,10 @@ UPDATE `SectorVocabulary` SET `Code`= 5 WHERE `Name`='National Taxonomy for Exem
 UPDATE `SectorVocabulary` SET `Code`= 2 WHERE `Name`='OECD Development Assistance Committee 3 digit codes';
 UPDATE `SectorVocabulary` SET `Code`= 4 WHERE `Name`='Statistical classification of economic activities in the European Community';
 UPDATE `SectorVocabulary` SET `Code`= 98 WHERE `Name`='International Organization for Standardization';
+
+UPDATE `SectorVocabulary` SET `Name`= 'OECD DAC CRS Purpose Codes (5 digit)' WHERE `Name`='OECD Development Assistance Committee';
+UPDATE `SectorVocabulary` SET `Name`= 'OECD DAC CRS Purpose Codes (3 digit)' WHERE `Name`='OECD Development Assistance Committee 3 digit codes';
+UPDATE `SectorVocabulary` SET `Name`= 'Reporting Organisation' WHERE `Name`='Reporting Organisation (i.e. maintained by the organisation referred to in the <reporting-org> element.';
+UPDATE `SectorVocabulary` SET `Name`= 'Reporting Organisation2' WHERE `Name`='International Organization for Standardization';
+
+DELETE FROM `SectorVocabulary` WHERE `Name`= "World Bank";
